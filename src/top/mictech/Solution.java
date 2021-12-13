f1cd1877657afc69f97c6b470427e4bedbe7c009
qingyu-sama <1470078125@qq.com>
2021-12-13 14:49:22 +0800

    保持城市天际线

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -1,4 +1,81 @@
 package top.mictech;
 
 public class Solution {
+    // region 一般题
+
+    // 807. 保持城市天际线
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int[] h = new int[grid.length];
+        int[] w = new int[grid[0].length];
+        int c = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j : grid[i])
+                h[i] = Math.max(h[i], j);
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                w[i] = Math.max(grid[j][i], w[i]);
+            }
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                c += (Math.min(h[i], w[j]) - grid[i][j]);
+            }
+        }
+        return c;
+    }
+
+    // endregion
 }
+
+//region 题目提供的数据结构
+
+class ListNode {
+    ListNode next;
+    private int val;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+//endregion
