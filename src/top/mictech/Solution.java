3b2f67831cfd823c76b387a4da662f7192868954
qingyu-sama <1470078125@qq.com>
2021-12-15 14:29:59 +0800

    只出现一次的数字

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -58,6 +58,13 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 136. 只出现一次的数字
+    public int singleNumber(int[] nums) {
+        int p = 0;
+        for (int i : nums) p ^= i;
+        return p;
+    }
+
     // 130. 被围绕的区域
     public void solve(char[][] board) {
         boolean[][] zt = new boolean[board.length][board[0].length];
