ea35f6c2b54a5f81111a09bb41e301afa4a507ec
qingyu-sama <1470078125@qq.com>
2021-12-15 13:55:28 +0800

    搜索旋转排序数组

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -112,6 +112,23 @@ public class Solution {
         return false;
     }
 
+    // 33. 搜索旋转排序数组
+    public int search(int[] nums, int target) {
+        int l = 0, m, r = nums.length - 1;
+        while (l <= r) {
+            m = l + r >> 1;
+            if (nums[m] == target) return m;
+            if (nums[m] >= nums[0])
+                if (nums[m] < target) l = m + 1;
+                else if (target >= nums[0]) r = m - 1;
+                else l = m + 1;
+            else if (nums[m] > target) r = m - 1;
+            else if (target >= nums[0]) r = m - 1;
+            else l = m + 1;
+        }
+        return -1;
+    }
+
     // 20. 有效的括号
     public boolean isValid(String s) {
         if (s.length() == 0) return true;
