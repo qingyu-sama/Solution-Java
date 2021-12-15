f9bd49ea7e07a0e91ba59b9f30c18dd35008b59b
qingyu-sama <1470078125@qq.com>
2021-12-15 14:21:22 +0800

    爬楼梯

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -112,6 +112,17 @@ public class Solution {
         return false;
     }
 
+    // 70. 爬楼梯
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+        int p1 = 2, p2 = 3;
+        for (int i = 3; i < n; i++) {
+            p2 += p1;
+            p1 = p2 - p1;
+        }
+        return p2;
+    }
+
     // 66. 加一
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
