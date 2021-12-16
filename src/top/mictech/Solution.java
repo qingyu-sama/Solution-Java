f436d71c35734c912b7b6d45e70b77e379db3115
qingyu-sama <1470078125@qq.com>
2021-12-16 16:21:01 +0800

    左旋转字符串

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -58,6 +58,11 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 剑指 Offer 58 - II. 左旋转字符串
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0, n);
+    }
+
     // 剑指 Offer 05. 替换空格
     public String replaceSpace(String s) {
         StringBuilder sb = new StringBuilder();
