0848f0905281d2184e47be55608aefb3fd02d158
qingyu-sama <1470078125@qq.com>
2021-12-16 16:19:23 +0800

    替换空格

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -58,6 +58,17 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 剑指 Offer 05. 替换空格
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') sb.append("%20");
+            else sb.append(c);
+        }
+        return sb.toString();
+    }
+
     // 1816. 截断句子
     public String truncateSentence(String s, int k) {
         StringBuilder sb = new StringBuilder();
