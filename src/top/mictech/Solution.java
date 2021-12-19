2cb1d9089859db3ad412e4567c6c0d3d85f9fb9b
qingyu-sama <1470078125@qq.com>
2021-12-19 21:45:39 +0800

    相交链表

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -1206,6 +1206,21 @@ public class Solution {
     }
 
     // endregion
+
+    // region 链表题
+
+    // 160. 相交链表
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode l1 = headA, l2 = headB;
+        while (l1 != l2) {
+            l1 = l1 == null ? headB : l1.next;
+            l2 = l2 == null ? headA : l2.next;
+        }
+        return l1;
+    }
+
+    // endregion
 }
 
 // region 数据结构设计题
