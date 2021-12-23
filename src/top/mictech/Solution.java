7879647e3b460a59d9adc6edfe5236ee77ebb833
qingyu-sama <1470078125@qq.com>
2021-12-23 13:59:52 +0800

    将有序数组转换为二叉搜索树

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -66,6 +66,20 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 108. 将有序数组转换为二叉搜索树
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int l, int r) {
+        if (l > r) return null;
+        int m = l + r >> 1;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = sortedArrayToBST(nums, l, m - 1);
+        root.right = sortedArrayToBST(nums, m + 1, r);
+        return root;
+    }
+
     // 1823. 找出游戏的获胜者
     public int findTheWinner(int n, int k) {
         class Node {
