529085a7a0f8325488cedc0eb2db6fc33e236c65
qingyu-sama <1470078125@qq.com>
2022-01-03 16:43:30 +0800

    一周中的第几天

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -72,6 +72,16 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 1185. 一周中的第几天
+    public String dayOfTheWeek(int day, int month, int year) {
+        final String[] week = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        if (month == 1 || month == 2) {
+            month += 12;
+            year--;
+        }
+        return week[(day + 2 * month + 3 * (month + 1) / 5 + year + year / 4 - year / 100 + year / 400 + 1) % 7];
+    }
+
     // 191. 位1的个数
     public int hammingWeight(int n) {
         int ans = 0;
