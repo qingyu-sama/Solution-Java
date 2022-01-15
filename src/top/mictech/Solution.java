7548d73999113d7d2648691a047f5298fec4cea0
qingyu-sama <1470078125@qq.com>
2022-01-15 20:42:16 +0800

    计算力扣银行的钱

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -73,6 +73,12 @@ class Node {
 public class Solution {
     // region 一般题
 
+    // 1716. 计算力扣银行的钱
+    public int totalMoney(int n) {
+        int a = n / 7, b = n % 7;
+        return ((49 + a * 7) * a + (a * 2 + b + 1) * b) >> 1;
+    }
+
     // 1091. 二进制矩阵中的最短路径(未完成，超内存限制)
     public int shortestPathBinaryMatrix(int[][] grid) {
         Stack<int[]> stack1 = new Stack<>();
