b17ecbaec8dbe6883668e404abb61f035dc94794
qingyu-sama <1470078125@qq.com>
2022-01-18 20:25:27 +0800

    二叉搜索树的最近公共祖先

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -1908,26 +1908,10 @@ public class Solution {
 
     // 235. 二叉搜索树的最近公共祖先
     public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-        Set<TreeNode> set = new HashSet<>();
-        lowestCommonAncestorDFS(root, set, p);
-        return lowestCommonAncestorDFS(root, q, set);
-    }
-
-    private void lowestCommonAncestorDFS(TreeNode root, Set<TreeNode> set, TreeNode p) {
-        set.add(root);
-        if (root == p) return;
-        if (root.val > p.val) lowestCommonAncestorDFS(root.left, set, p);
-        else lowestCommonAncestorDFS(root.right, set, p);
-    }
-
-    private TreeNode lowestCommonAncestorDFS(TreeNode root, TreeNode q, Set<TreeNode> set) {
-        TreeNode result = null;
-        if (set.contains(root)) result = root;
-        if (root == q) return result;
-        TreeNode temp = null;
-        if (root.val > q.val) temp = lowestCommonAncestorDFS(root.left, q, set);
-        else temp = lowestCommonAncestorDFS(root.right, q, set);
-        return temp == null ? result : temp;
+        while (true)
+            if ((p.val >= root.val && q.val <= root.val) || (q.val >= root.val && p.val <= root.val)) return root;
+            else if (p.val > root.val) root = root.right;
+            else root = root.left;
     }
 
     // 103. 二叉树的锯齿形层序遍历
