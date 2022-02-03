422684b917bd8cad30c6f0d27b2ba0b02edd3233
qingyu-sama <1470078125@qq.com>
2022-02-03 19:37:33 +0800

    和为 K 的最少斐波那契数字数目

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -75,6 +75,26 @@ public class Solution {
 
     // region 一般题
 
+    // 1414. 和为 K 的最少斐波那契数字数目
+    private static final int[] findMinFibonacciNumbersFibs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733};
+
+    public int findMinFibonacciNumbers(int k) {
+        int ans = 0;
+        while (k != 0) {
+            int l = 0, r = findMinFibonacciNumbersFibs.length - 1, m = 0;
+            while (l <= r) {
+                m = l + r >> 1;
+                if (findMinFibonacciNumbersFibs[m] <= k && (m == findMinFibonacciNumbersFibs.length - 1 || findMinFibonacciNumbersFibs[m + 1] > k))
+                    break;
+                else if (findMinFibonacciNumbersFibs[m] > k) r = m - 1;
+                else l = m + 1;
+            }
+            k -= findMinFibonacciNumbersFibs[m];
+            ans++;
+        }
+        return ans;
+    }
+
     // 2000. 反转单词前缀
     public String reversePrefix(String word, char ch) {
         char[] cs = new char[word.length()];
