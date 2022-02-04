61191b3b577f22c867cb494bd8ae12a0dda5e2fb
qingyu-sama <1470078125@qq.com>
2022-02-04 17:33:49 +0800

    可以形成最大正方形的矩形数目

--- src/top/mictech/Solution.java
+++ src/top/mictech/Solution.java
@@ -75,6 +75,19 @@ public class Solution {
 
     // region 一般题
 
+    // 1725. 可以形成最大正方形的矩形数目
+    public int countGoodRectangles(int[][] rectangles) {
+        int max = 0, ans = 0;
+        for (int[] is : rectangles) {
+            int l = Math.min(is[0], is[1]);
+            if (l > max) {
+                max = l;
+                ans = 1;
+            } else if (l == max) ans++;
+        }
+        return ans;
+    }
+
     // 1414. 和为 K 的最少斐波那契数字数目
     private static final int[] findMinFibonacciNumbersFibs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733};
 
